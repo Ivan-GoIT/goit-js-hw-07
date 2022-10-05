9e38e49d42d2c1b5b8f10e051eead729689bfc7e
IvanKoshel98 <GitHubPassvord142>
2022-10-05 16:07:02 +0300

    task7_2 create title

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+
+galleryContainerEl.innerHTML = createGalleryMarkup(galleryItems);
+
+[...galleryContainerEl.getElementsByTagName('a')].map(el => { el.dataset.lightbox = 'gallery'; el.dataset.title= })
+//const gallery=new SimpleLightbox('show.simplelightbox',)
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a 
+        class="gallery__item" 
+      href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
