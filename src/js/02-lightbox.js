91c7b2a712a7b2e8a98788e2be4f35c2d5bda950
Ivan-GoIT <educationzak@gmail.com>
2022-10-05 21:37:56 +0300

    ligtbox не здается

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -5,19 +5,26 @@ const galleryContainerEl = document.querySelector(".gallery");
 
 galleryContainerEl.innerHTML = createGalleryMarkup(galleryItems);
 
-[...galleryContainerEl.getElementsByTagName('a')].map(el => { el.dataset.lightbox = 'gallery'; el.dataset.title= })
-//const gallery=new SimpleLightbox('show.simplelightbox',)
+addAttributesForSimpleLightbox(galleryContainerEl);
+//const lightbox = new SimpleLightbox({ elements: ".gallery > li > a" });
 
 function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a 
+      return `<li><a 
         class="gallery__item" 
       href="${original}">
   <img class="gallery__image" 
   src="${preview}" 
   alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join("");
+}
+
+function addAttributesForSimpleLightbox(galleryEl) {
+  [...galleryEl.getElementsByTagName("a")].map((el) => {
+    el.dataset.lightbox = "gallery";
+    el.dataset.title = el.getElementsByTagName("img")[0].alt;
+  });
 }
